bfdc651e97a2a050cf0ec241cb30d9555e5946fd
mhutch <m.j.hutchinson@gmail.com>
2016-03-12 15:05:07 -0800

    Final updates

--- Assets/Assets.cs
+++ Assets/Assets.cs
@@ -16,9 +16,6 @@ public partial class Assets
 			public const string Block3 = "Materials/Block3.xml";
 			public const string Block4 = "Materials/Block4.xml";
 			public const string Block5 = "Materials/Block5.xml";
-			public const string Mushroom = "Materials/Mushroom.xml";
-			public const string Stone = "Materials/Stone.xml";
-			public const string Terrain = "Materials/Terrain.xml";
 			public const string Terrain2 = "Materials/Terrain2.xml";
 			public const string VehicleHull = "Materials/VehicleHull.xml";
 			public const string VehicleWheel = "Materials/VehicleWheel.xml";
@@ -27,7 +24,6 @@ public partial class Assets
 	public static class Models
 	{
 			public const string Box = "Models/Box.mdl";
-			public const string Mushroom = "Models/Mushroom.mdl";
 			public const string Sphere = "Models/Sphere.mdl";
 	}
 
@@ -58,14 +54,7 @@ public partial class Assets
 			public const string Block4 = "Textures/Block4.png";
 			public const string Block5 = "Textures/Block5.png";
 			public const string HeightMap = "Textures/HeightMap.png";
-			public const string Mushroom = "Textures/Mushroom.dds";
-			public const string StoneDiffuse = "Textures/StoneDiffuse.dds";
-			public const string StoneNormal = "Textures/StoneNormal.dds";
 			public const string Terrain2 = "Textures/Terrain2.png";
-			public const string TerrainDetail1 = "Textures/TerrainDetail1.dds";
-			public const string TerrainDetail2 = "Textures/TerrainDetail2.dds";
-			public const string TerrainDetail3 = "Textures/TerrainDetail3.dds";
-			public const string TerrainWeights = "Textures/TerrainWeights.dds";
 			public const string UI = "Textures/UI.png";
 			public const string VehicleHull = "Textures/VehicleHull.png";
 			public const string VehicleWheel = "Textures/VehicleWheel.png";
